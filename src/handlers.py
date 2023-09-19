cc7628b4302426c8e1c4a4cdd3edab6080ca3b39
Kanatov Vladislav <99506862+lon8@users.noreply.github.com>
2023-09-19 13:21:51 +0300

    Update handlers.py

--- src/handlers.py
+++ src/handlers.py
@@ -6,24 +6,8 @@ import requests
 
 from src.celery_worker import process_task
 
-headers = {
-    'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjk0ODMwMTg5LCJpYXQiOjE2OTQ3NDM3ODksImp0aSI6ImFhYjBhZWIxNmQ2MzQ3OGViMjA4MDI3MmI5Y2JhNTRkIiwidXNlcl9pZCI6NTh9.4KBSbdjmterjxCAXRspSdAbh-b-MthS7-aBFyT9Hx7M',
-}
+@router.post('/') # Тут будет POST метод, потому что будем получать access_token, id или email
 
-json_data = {
-    'search_query': 'some_search_query',
-}
-
-router = APIRouter()
-
-# Пример 
-
-# data = {
-#   uid: our id
-# }
-
-@router.post('/') # Тут будет POST метод, потому что будем получать access_token,
-                         #id или email пользователя
 async def main_route(info : dict):
     # Отправляем задачу на выполнение в Celery
     result = process_task.delay(info)
